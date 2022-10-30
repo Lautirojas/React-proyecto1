244a8537edf4c4deb73a7d617eb2192e3bb9651d
Lautaro <lautarorojas572@gmail.com>
2022-10-30 20:52:38 -0300

    Añadiendo nuevo slide de productos relacionados

--- src/components/layouts/detalleProduct/detalleRecommend.jsx
+++ src/components/layouts/detalleProduct/detalleRecommend.jsx
@@ -1,6 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation, Scrollbar, A11y } from "swiper";
+import { getProducts } from "../../../utils/Firebase";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "../../../css/home.css";
 
 const DetalleRecommend = () => {
+  const [Productos, setProducto] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    getProducts().then((producto) => {
+      const productoscategoria = producto.filter(
+        (productos) => productos[1].categoria === "Buzos"
+      );
+      console.log(productoscategoria);
+      const cards = productoscategoria.map((producto) => {
+        let carlos = (
+          <Swiper
+            key={producto[0]}
+            className="mySwiper"
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            slidesPerView={3}
+            spaceBetween={30}
+            navigation={true}
+            scrollbar={{ hide: true }}
+            pagination={{ clickable: true }}
+          >
+            <SwiperSlide className="testimonio-recommended">
+              <div className="slide-recommended">
+                <Link to={`/productos/${producto[0]}`}>
+                  <div>
+                    <img src={producto[1].img} alt="" />
+                  </div>
+                </Link>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        );
+        return carlos;
+      });
+      setProducto(cards);
+    });
+  }, [id]);
   return (
     <>
       <div className="container-recommended">
@@ -8,7 +53,7 @@ const DetalleRecommend = () => {
           <div className="r-products-header">
             <h2 className="r-header-title">Productos relacionados</h2>
           </div>
-          <div className="r-products-carrousel"></div>
+          <div className="r-products-carrousel">{Productos}</div>
         </div>
       </div>
     </>
